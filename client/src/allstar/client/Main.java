b7ef2e39e5ab640cea1bb25325597744b73ef9a2
David Boschwitz <dbosch@iastate.edu>
2015-02-10 18:02:18 -0600

    Added commentary/javadoc

--- client/src/allstar/client/Main.java
+++ client/src/allstar/client/Main.java
@@ -3,17 +3,27 @@ package allstar.client;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
+ * Does most of the work for the client.
  *
  * @author davidboschwitz
  */
 public class Main {
 
+    /**
+     * The ip-address to attempt to connect to (Default
+     * <code>davidboschwitz.student.iastate.edu</code>.
+     */
     static String ip = "davidboschwitz.student.iastate.edu";
+    /**
+     * The port to attempt to connect to (Default <code>2727</code>).
+     */
     static int port = 2727;
+    /**
+     * The Client object that holds most of the data for the client.
+     */
     static Client client;
 
     /**
@@ -22,16 +32,18 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if(args.length > 0){
+        if (args.length > 0) {
             ip = args[0];
         }
+        /* The reader for System.in */
         BufferedReader line = new BufferedReader(new InputStreamReader(System.in));
+        /* The next line */
         String next = "";
         OutputStream out;
         try {
-            /** init client */
+            /* init client */
             client = new Client(new Socket(ip, port));
-            /** init the output stream to the server */
+            /* init the output stream to the server */
             out = client.socket.getOutputStream();
         } catch (java.net.UnknownHostException uhe) {
             uhe.printStackTrace();
@@ -40,8 +52,10 @@ public class Main {
             ioe.printStackTrace();
             return;
         }
+        /* Start the Process class */
         new Thread(new Process()).start();
         try {
+            /* waits for the next line of input into the prompt */
             while ((next = line.readLine()) != null) {
                 //System.out.println("[sent]: "+next);
                 out.write(next.getBytes());
