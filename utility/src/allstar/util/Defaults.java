6c40eb29274708aafba53e510035ad0090b57a71
David Boschwitz <dbosch@iastate.edu>
2015-02-16 01:50:51 -0600

    Added default AudioFormat

--- utility/src/allstar/util/Defaults.java
+++ utility/src/allstar/util/Defaults.java
@@ -1,23 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package allstar.util;
+
+import javax.sound.sampled.AudioFormat;
 
 /**
  * The class that holds defaults for the Client and Server projects.
+ *
  * @author davidboschwitz
  */
 public class Defaults {
-    
-    public final static javax.sound.sampled.AudioFormat AudioFormat = null;
-    
-    /** 
-     * You should never create an instance of this class. It is for static resources only.
+
+    /**
+     * The Default AudioFormat to use for all audio (to be consistent).
      */
-    private Defaults(){
+    public final static AudioFormat AudioFormat = new AudioFormat(16000.0F, 8, 2, true, true);
+
+    /**
+     * You should never create an instance of this class. It is for static
+     * resources only.
+     */
+    private Defaults() {
     }
-    
-    
 }
