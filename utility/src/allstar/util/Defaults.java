2f797708f21740c8eb285662ec55a3403042de7f
David Boschwitz <dbosch@iastate.edu>
2015-02-16 01:16:38 -0600

    Added Netbeans project for allstar.util

---
+++ utility/src/allstar/util/Defaults.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package allstar.util;
+
+/**
+ *
+ * @author davidboschwitz
+ */
+public class Defaults {
+    
+    /** 
+     * You should never create an instance of this class. It is for static resources only.
+     */
+    private Defaults(){
+    }
+    
+    
+}
