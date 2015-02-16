5f4ea39d77fc1fd9bf8289eeba6493f66170b5a4
David Boschwitz <dbosch@iastate.edu>
2015-02-16 02:13:55 -0600

    Added audio in/out (first try!)

--- client/src/allstar/client/Client.java
+++ client/src/allstar/client/Client.java
@@ -7,30 +7,41 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 import allstar.util.Defaults;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 
 /**
  * Holds most of the data for the client and connecting to the server.
  *
  * @author davidboschwitz
  */
-public class Client {
+public class Client implements LineListener {
 
     /**
      * The socket connection to the client
      */
     public Socket socket;
+    /**
+     * Audio output clip?
+     */
+    private Clip clip;
 
     public Client(Socket s) {
         this.socket = s;
     }
 
-    public void audio() {
+    public void audioOut() {
         boolean stopped = false;
         TargetDataLine line;
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, Defaults.AudioFormat); // format is an AudioFormat object
         if (!AudioSystem.isLineSupported(info)) {
             // Handle the error ... 
-            throw new UnsupportedOperationException("FUCK IDK WHAT TO DO");
+            System.out.println("Audio System not supported.");
+            throw new UnsupportedOperationException("Audio System not supported (FUCK)");
         }
         // Obtain and open the line.
         try {
@@ -59,5 +70,62 @@ public class Client {
             out.write(data, 0, numBytesRead);
         }
 
+    }
+
+    public void audioIn(InputStream in) {
+        //JFileChooser chooser = new JFileChooser();
+        //chooser.showOpenDialog(null);
+        //soundFile = chooser.getSelectedFile();
+
+        //System.out.println("Playing " + soundFile.getName());
+        Line.Info linfo = new Line.Info(Clip.class);
+        Line line;
+        try {
+            line = AudioSystem.getLine(linfo);
+        } catch (javax.sound.sampled.LineUnavailableException lue) {
+            lue.printStackTrace();
+            return;
+        }
+
+        clip = (Clip) line;
+        clip.addLineListener(this);
+        AudioInputStream ais;
+        try {
+            ais = AudioSystem.getAudioInputStream(in);
+        } catch (javax.sound.sampled.UnsupportedAudioFileException uafe) {
+            uafe.printStackTrace();
+            return;
+        } catch (java.io.IOException ioe) {
+            ioe.printStackTrace();
+            return;
+        }
+        try {
+            clip.open(ais);
+        } catch (javax.sound.sampled.LineUnavailableException lue) {
+            lue.printStackTrace();
+            return;
+        } catch (java.io.IOException ioe) {
+            ioe.printStackTrace();
+            return;
+        }
+        clip.start();
+    }
+
+    @Override
+    public void update(LineEvent le) {
+        LineEvent.Type type = le.getType();
+        if (type == LineEvent.Type.OPEN) {
+            System.out.println("OPEN");
+        } else if (type == LineEvent.Type.CLOSE) {
+            System.out.println("CLOSE");
+            System.exit(0);
+        } else if (type == LineEvent.Type.START) {
+            System.out.println("START");
+            //playingDialog.setVisible(true);
+        } else if (type == LineEvent.Type.STOP) {
+            System.out.println("STOP");
+            //playingDialog.setVisible(false);
+            clip.close();
+        }
     }
 }
