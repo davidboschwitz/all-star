3cb6b121e8eceac67850fe8d0e613d9ea52bdf98
David Boschwitz <dbosch@iastate.edu>
2015-02-16 01:56:38 -0600

    Added basic defaults

--- utility/src/allstar/util/Defaults.java
+++ utility/src/allstar/util/Defaults.java
@@ -13,7 +13,17 @@ public class Defaults {
      * The Default AudioFormat to use for all audio (to be consistent).
      */
     public final static AudioFormat AudioFormat = new AudioFormat(16000.0F, 8, 2, true, true);
-
+    
+    /**
+     * The Default Server IP-Address to connect to.
+     */
+    public static final String ServerIP = "davidboschwitz.student.iastate.edu";
+    
+    /**
+     * The Default Server port number to connect to.
+     */
+    public static final int port = 2727;
+    
     /**
      * You should never create an instance of this class. It is for static
      * resources only.
