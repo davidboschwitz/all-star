4b4650ae086c384e839303ed956a27a3c774d6dd
David Boschwitz <dbosch@iastate.edu>
2015-03-15 22:28:45 -0500

    Added run script

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# all-star IRLP Node RaspberryPi Update Script
+# Made by David Boschwitz
+# Updated 3/10/2015
+
+cd client/dist
+java -cp All-Star_Client.jar:lib/All-Star_Utilities.jar allstar.client.Main
