30632ef2d512c3684f23de322929c26a93d70436
David Boschwitz <hihid27@aim.com>
2015-04-03 14:08:10 -0500

    Added MAX_CLIENTS variable and documentation.

--- utility/src/allstar/util/Defaults.java
+++ utility/src/allstar/util/Defaults.java
@@ -14,35 +14,44 @@ public class Defaults {
      * The Default AudioFormat to use for all audio (to be consistent).
      */
     public final static AudioFormat AudioFormat = new AudioFormat(16000.0F, 8, 2, true, true);
-    
+
     /**
      * The Default AudioFileFormat to use for all audio.
      */
     public final static AudioFileFormat.Type FileFormatType = AudioFileFormat.Type.AU;
+    
     /**
      * The Default Server IP-Address to connect to.
      */
-    public static final String ServerIP = "davidboschwitz.student.iastate.edu";
+    public static final String SERVER_IP = "davidboschwitz.student.iastate.edu";
 
     /**
      * The Default Server port number to connect to.
      */
-    public static final int port = 2727;
+    public static final int SERVER_PORT = 2727;
 
     /**
      * The buffer size for audio.
      */
     public static final int EXTERNAL_BUFFER_SIZE = 128000;
+   
     /**
-     *
+     * Is the text version of the client enabled (audio must be disabled for
+     * this to work). Disabled by default.
      */
-    public static final boolean TextEnabled = false;
+    public static final boolean TEXT_ENABLED = false;
 
     /**
-     *
+     * Is the audio version of the client enabled (this must be disabled for
+     * text to work). Enabled by default.
      */
-    public static final boolean AudioEnabled = true;
+    public static final boolean AUDIO_ENABLED = true;
 
+    /**
+     * The maximum number of clients that will be allowed to connect.
+     */
+    public static final int MAX_CLIENTS = 20;
+    
     /**
      * You should never create an instance of this class. It is for static
      * resources only.
