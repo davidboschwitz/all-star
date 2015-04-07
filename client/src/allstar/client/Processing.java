d314b83a6f6411cf219c6ff80b6f048733790841
David Boschwitz <hihid27@aim.com>
2015-04-07 18:08:39 -0500

    Renamed Process to Processing to resolve conflicts

---
+++ client/src/allstar/client/Processing.java
@@ -0,0 +1,53 @@
+package allstar.client;
+
+import allstar.util.Defaults;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Second thread
+ *
+ * @author davidboschwitz
+ */
+public class Processing implements Runnable {
+
+    @Override
+    public void run() {
+        InputStream in;
+        try {
+            in = Main.client.socket.getInputStream();
+        } catch (java.io.IOException ioe) {
+            ioe.printStackTrace();
+            return;
+        }
+        if (Defaults.AUDIO_ENABLED) {
+            try {
+                while (true) {
+                    if (in.available() > 0) {
+                        GPIO.GPIO_OUTPUT_START();
+                        Main.client.player.play(in);
+                    } else {
+                        GPIO.GPIO_OUTPUT_STOP();
+                    }
+                }
+            } catch (java.io.IOException ioe) {
+                ioe.printStackTrace();
+                return;
+            }
+        }
+        if (Defaults.TEXT_ENABLED) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String next = "";
+            try {
+                while ((next = reader.readLine()) != null) { //wait for next line of input from server
+                    System.out.println(next);
+                }
+            } catch (java.io.IOException ioe) {
+                ioe.printStackTrace();
+                return;
+            }
+        }
+    }
+
+}
